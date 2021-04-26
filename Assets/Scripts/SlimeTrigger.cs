97fb587ef9975e217ce1ae2f786bc5cc3ddb4241
Jordy_Rodrigues <jordyrg616@gmail.com>
2021-04-26 06:21:31 -0300

    Adicionando interface e habilidades à cena com geração de level;

--- Assets/Scripts/SlimeTrigger.cs
+++ Assets/Scripts/SlimeTrigger.cs
@@ -7,11 +7,11 @@ public abstract class SlimeTrigger : MonoBehaviour
 
     protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("alegoria");
+        //Debug.Log("alegoria");
 
         if (collision.gameObject.CompareTag(slimeTag))
         {
-            Debug.Log("margarida");
+            //Debug.Log("margarida");
 
             OnSlime(collision.gameObject.GetComponent<Slime>());
         }
